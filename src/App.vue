6c678f0b8c6cbe9031393832d0629c2ad498ded4
themrb8 <officialroman786@gmail.com>
2022-12-28 10:51:39 +0600

    api setup

--- src/App.vue
+++ src/App.vue
@@ -1,34 +1,7 @@
 <script>
 export default {
-  name: 'app',
-  data() {
-    return {
-      msg: 'Welcome to Your Vue.js App',
-      courses: [
-        {
-          id: 1,
-          title: 'VueJS',
-          description: 'The demo description for vuejs',
-        },
-        {
-          id: 2,
-          title: 'Laravel',
-          description: 'The demo description for Laravel',
-        },
-        {
-          id: 3,
-          title: 'NuxtJS',
-          description: 'The demo description for NuxtJS',
-        },
-      ],
-    }
-  },
-  methods: {
-    test() {
-      this.msg = 'changed';
-    }
-  }
-};
+  
+}
 </script>
 
 <template>
@@ -40,18 +13,4 @@ export default {
   </header>
 
   <RouterView></RouterView>
-
-  <div class="container mx-auto">
-    <h2 class="text-4xl font-bold text-state-900">{{ msg }}</h2>
-  <button class="bg-red-500 text-white py-2 px-6 text-xl rounded mt-6" @click="test">Test</button>
-
-
-    <h2 class="mt-8 text-2xl font-bold text-state-900">Courses</h2>
-
-
-    <div v-for="course in courses" class="mt-2 shadow p-4 mb-4 rounded-lg border border-gray-500">
-      <h2 class="text-xl font-bold text-state-900">{{ course.title }}</h2>
-      <p>{{ course.description }}</p>
-    </div>
-  </div>
 </template>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,25 @@
 <script>
-
+export default {
+    name: 'Home',
+    data() {
+        return {
+            todos: []
+        }
+    },
+    mounted() {
+        fetch('https://jsonplaceholder.typicode.com/todos/')
+        .then(response => response.json())
+        .then(json => this.todos = json)
+    }
+}
 </script>
 
 <template>
-    Home
+    <div class="container mx-auto">
+        <div v-for="todo in todos" class="flex-wrap justify-between items-center bg-slate-300 p-2 rounded-sm mb-4">
+            <div class="item">
+                <h3 class="font-semibold ">{{ todo.title }}</h3>
+            </div>
+        </div>
+    </div>
 </template>
